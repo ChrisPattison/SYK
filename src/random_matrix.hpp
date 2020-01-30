725378f9d6b7f84f1c8e151a75d024557521a7f5
Chris Pattison <chpattison@gmail.com>
2020-01-30 13:30:27 -0800

    Fix random matrix generation

--- src/random_matrix.hpp
+++ src/random_matrix.hpp
@@ -29,13 +29,13 @@ MatrixType RandomRealIidGauss(rng_type* rand_gen, int n, int m) {
 
 template<typename rng_type>
 MatrixType RandomGOE(rng_type* rand_gen, int n) {
-    auto A = RandomRealIidGauss(rand_gen, n, n);
+    MatrixType A = RandomRealIidGauss(rand_gen, n, n);
     return (A + A.transpose()) / 2;
 }
 
 template<typename rng_type>
 MatrixType RandomGUE(rng_type* rand_gen, int n) {
-    auto A = RandomRealIidGauss(rand_gen, n, n) + 1.0i * RandomRealIidGauss(rand_gen, n, n);
+    MatrixType A = RandomRealIidGauss(rand_gen, n, n) + 1.0i * RandomRealIidGauss(rand_gen, n, n);
     return (A + A.adjoint()) / 2;
 }
 }
