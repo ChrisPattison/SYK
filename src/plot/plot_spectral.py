ca9e651bec4d5cd796652ed3e82559dd723aa2ca
Chris Pattison <chpattison@gmail.com>
2020-06-10 13:42:47 -0700

    Update plotting script

--- src/plot/plot_spectral.py
+++ src/plot/plot_spectral.py
@@ -1,35 +1,90 @@
 #!/usr/bin/env python3
 import sys
+import matplotlib.style
+import matplotlib as mpl
+mpl.style.use('classic')
 import matplotlib.pyplot as plt
+
 import numpy as np 
 import pandas as pd 
 import flatbuffers
+import time as ti
 from numba import njit, prange
 from SYKSchema.Output import Output
+from SYKSchema.Checkpoint import Checkpoint
 
-output = Output.GetRootAsOutput(bytearray(open(sys.argv[1], 'rb').read()), 0)
-assert(not output.DataIsNone())
+def get_ssf_from_file(filename, time):
+    '''Attempt to read as output file and then as a checkpoint.
+    Our buffers don't have identifiers for some reason so OutputBufferHasIdentifier always gives false.
+    Fortunately, it blows up if we attempt to read it as Output and it's not.'''
 
-print('Total Compute: {}s'.format(output.TotalCompute()))
+    buffer = bytearray(open(filename, 'rb').read())
 
-@njit(nogil=True, parallel=True)
-def spectral_form_factor(time, data):
-    g = np.zeros_like(time)
-    for i in prange(len(time)):
-        g[i] = np.sum(np.exp(data * time[i]))
-    g = np.conj(g) * g
-    return g
+    try:
+        output = Output.GetRootAsOutput(buffer, 0)
+        assert(not output.DataIsNone())
+        spectral = spectral_form_factor(output, time)
+    except:
+        output = Checkpoint.GetRootAsCheckpoint(buffer, 0).Output()
+        assert(not output.DataIsNone())
+        spectral = spectral_form_factor(output, time)
 
-time = np.geomspace(1e-1, 1e6, 4000, dtype=np.dtype('c16'))*1.0j
+    return spectral
+            
 
-spectral = np.zeros_like(time)
+def spectral_form_factor(output, time):
 
-for point in (output.Data(i) for i in range(output.DataLength())):
-    spectral += spectral_form_factor(time, point.EigenvalsAsNumpy())
+    print('Total Compute: {}s'.format(output.TotalCompute()))
 
-spectral = spectral / (output.DataLength()**2)
-plt.yscale('log')
-plt.xscale('log')
-plt.plot(np.imag(time), np.real(spectral))
-plt.savefig('plot.pdf')
+    @njit(nogil=True, parallel=True)
+    def spectral_form_factor(time, data):
+        g = np.zeros_like(time)
+        for i in prange(len(time)):
+            g[i] = np.sum(np.exp(data * time[i]))
+        g = np.conj(g) * g
+        return g
+
+    spectral = np.zeros_like(time)
+
+    for point in (output.Data(i) for i in range(output.DataLength())):
+        spectral += spectral_form_factor(time, point.EigenvalsAsNumpy())
+
+    spectral = spectral / (output.DataLength()**2)
+
+    return spectral, output.DataLength()
+
+def load_from_checkpoint(filename):
+    checkpoint = Checkpoint.GetRootAsCheckpoint(bytearray(open(filename, 'rb').read()), 0)
+    assert(not checkpoint.OutputIsNone())
+    return checkpoint.output
+
+def pretty_label(filename):
+    '''Attempt to get a nice plot label from our naming convention'''
+    try:
+        parts = filename.split('/')[-1].split('.')
+        assert(parts[-1] == 'bin')
+        return parts[-2]
+    except:
+        return filename
+
+if __name__ == '__main__':
+    time = np.geomspace(1e-1, 1e6, 4000, dtype=np.dtype('c16'))*1.0j
+    output_files = sys.argv[1:]
+
+    for filename in output_files:
+        label = pretty_label(filename)
+        print(f'Plotting {label}')
+
+        timestart = ti.time()
+        spectral, points = get_ssf_from_file(filename, time)
+        print(f'Plotted {label} in {ti.time() - timestart}')
+        plt.yscale('log')
+        plt.xscale('log')
+        plt.plot(np.imag(time), np.real(spectral), label=f'${label}~~N={points}$')
+
+    plt.grid()
+    plt.legend()
+    plt.savefig('spectral.pdf')
+
+
 
