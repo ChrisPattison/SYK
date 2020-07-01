ea4db07e7a1b9d97902842c17ec776b6f64c584a
Chris Pattison <chpattison@gmail.com>
2020-07-01 11:48:21 -0700

    Separate pauli manipulation from fermion representation

---
+++ src/pauli.hpp
@@ -0,0 +1,170 @@
+#pragma once
+#include <cassert>
+#include <algorithm>
+#include <utility>
+#include <complex>
+#include <cstdint>
+#include <exception>
+
+#include <Eigen/Dense>
+#include "syk_types.hpp"
+#include "util.hpp"
+
+/** Utilities for working with a Hamiltonian as a sum of Pauli operators
+ */
+namespace syk {
+using namespace std::complex_literals;
+
+/** Pauli group
+ */
+
+enum class AbstractPauli : short {I = 0, X = 1, Y = 2, Z = 3};
+enum class Sign : short {P = 1, M = -1};
+struct Pauli { AbstractPauli m; Sign s; };
+
+Sign operator*(Sign a, Sign b) {
+    return (a == b) ? Sign::P : Sign::M;
+}
+
+Sign operator!(Sign a) {
+    return a * Sign::M;
+}
+
+Pauli operator*(Pauli a, Pauli b) {
+    auto new_sign = a.s * b.s;
+    if( !(static_cast<int>(a.m) <= static_cast<int>(b.m)) ) {
+        std::swap(a, b);
+        new_sign = (
+                a.m == AbstractPauli::I 
+            ||  b.m == AbstractPauli::I
+            ||  a.m == b.m) ? new_sign : !new_sign;
+    }
+    
+    if (a.m == AbstractPauli::I) { return Pauli {b.m, new_sign}; }
+    if (a.m == b.m) { return Pauli {AbstractPauli::I, new_sign}; }
+    return Pauli {
+        static_cast<AbstractPauli>(3 - (static_cast<int>(a.m) + static_cast<int>(b.m))%3),
+        new_sign};
+}
+
+/** Utilities for construction Hamiltonian as a sum of products of Paulis
+ * This has a hard coded maximum number of qubits (20) for performance
+ * If/When increased it should be a multiple of 4 to allow vectorization
+ * 
+ * Constructing the full Hamiltonian is done coefficient-wise by going through each term in the sum
+ * For each term we stop when we hit a 0
+ * This is branch heavy but was about 2x faster than without the branch
+ */
+struct PauliRep {
+    static constexpr int kmax_qubits = 20;
+    using PauliString = std::array<Pauli, kmax_qubits>;
+    using PauliMatrix = Eigen::Matrix<std::complex<short>, 2, 2>;
+
+    struct Term {
+        PauliString pauli_op;
+        double weight;
+    };
+
+protected:
+    int num_qubits_;
+
+public:
+    PauliMatrix m_[4];
+
+    PauliRep(int num_qubits) : num_qubits_(num_qubits) {
+        if(num_qubits > kmax_qubits) { throw std::runtime_error("Number of qubits exceeds maximum support. Increase the compile time value"); }
+
+        m_[0] << 1, 0,
+                0, 1;
+
+        m_[1] << 0, 1,  
+                1, 0;
+
+        m_[2] << 0, -1i,
+                1i, 0;
+
+        m_[3] << 1, 0,
+                0, -1;
+    }
+
+    /** Get sign
+     */
+    short repp(Sign a) const __attribute__((always_inline)) {
+        return static_cast<short>(a);
+    }
+
+    /** Get matrix for a one qubit Pauli
+     */
+    const PauliMatrix& repp(AbstractPauli a) const __attribute__((always_inline)) {
+        return m_[static_cast<short>(a)];
+    }
+
+    auto get_repp_factor(Pauli a) const __attribute__((always_inline)) {
+        return repp(a.m) * repp(a.s);
+    }
+
+    Pauli PauliI() const { return Pauli {AbstractPauli::I, Sign::P}; }
+    Pauli PauliX() const { return Pauli {AbstractPauli::X, Sign::P}; }
+    Pauli PauliY() const { return Pauli {AbstractPauli::Y, Sign::P}; }
+    Pauli PauliZ() const { return Pauli {AbstractPauli::Z, Sign::P}; }
+
+    /** Single Pauli matrix element
+     */
+    std::complex<short> get_pauli_element(int i, int j, Pauli p) const __attribute__((always_inline)) {
+        return repp(p.m)(i,j) * repp(p.s);
+    }
+
+    /** Convert a short taking values 0, +/- 1 to double
+     */
+    std::complex<double> zero_one_short_to_double(std::complex<short> a) const {
+        return std::complex<double>(
+            a.real() == 0 ? 0.0 : (a.real() > 0 ? 1.0 : -1.0),
+            a.imag() == 0 ? 0.0 : (a.imag() > 0 ? 1.0 : -1.0)
+        );
+    }
+
+    /** Get matrix element of a string of Paulis
+     * shorts everywhere to hopefully let the compiler do its thing
+     */
+    std::complex<double> get_matrix_element(int i, int j, const PauliString& string) {
+        std::complex<short> acc(1,0);
+        int max_level = num_qubits_ - 1;
+
+        #pragma GCC unroll 4
+        for(int level = 0; level <= max_level; ++level) {
+            int mask = 1 << level;
+            int sub_i = (i & mask) >> level;
+            int sub_j = (j & mask) >> level;
+            acc *= get_pauli_element(sub_i, sub_j, string[level]);
+            if(acc.real() == 0 && acc.imag() == 0) { break; }
+        }
+        return zero_one_short_to_double(acc);
+    }
+
+    /** Compute the Hamiltonian for some sum of terms
+     * Give only the cols/rows specified by subspace
+     * Subspace is usually 0 to (2^N - 1) but could be something else where symmetries exist
+     */
+    MatrixType get_hamiltonian(std::vector<Term> terms, std::vector<std::uint64_t> subspace) {
+        MatrixType hamiltonian = MatrixType::Zero(subspace.size(), subspace.size());
+
+        #pragma omp parallel for
+        for(int j = 0; j < subspace.size(); ++j) {
+            for(int i = 0; i < subspace.size(); ++i) {
+                double matrix_element_re = 0.0;
+                double matrix_element_im = 0.0;
+                auto num_terms = terms.size();
+
+                for(int k = 0; k < num_terms; ++k) {
+                    auto single_element = get_matrix_element(subspace[i], subspace[j], terms[k].pauli_op);
+                    matrix_element_re += terms[k].weight * single_element.real();
+                    matrix_element_im += terms[k].weight * single_element.imag();
+                }
+                hamiltonian(i,j) = std::complex<double>(matrix_element_re, matrix_element_im);
+            }
+        }
+
+        return hamiltonian;
+    }
+};
+}
--- src/syk.hpp
+++ src/syk.hpp
@@ -8,95 +8,31 @@
 #include <cmath>
 
 #include <Eigen/Dense>
-#include <unsupported/Eigen/KroneckerProduct>
 #include "syk_types.hpp"
+#include "pauli.hpp"
 #include "util.hpp"
+
+/** Generation for SYK Hamiltonians
+ */
 
 namespace syk {
 using namespace std::complex_literals;
 
-// Fermion and Pauli group representations
-
-enum class AbstractPauli : short {I = 0, X = 1, Y = 2, Z = 3};
-enum class Sign : short {P = 1, M = -1};
-struct Pauli { AbstractPauli m; Sign s; };
-
-Sign operator*(Sign a, Sign b) {
-    return (a == b) ? Sign::P : Sign::M;
-}
-
-Sign operator!(Sign a) {
-    return a * Sign::M;
-}
-
-Pauli operator*(Pauli a, Pauli b) {
-    auto new_sign = a.s * b.s;
-    if( !(static_cast<int>(a.m) <= static_cast<int>(b.m)) ) {
-        std::swap(a, b);
-        new_sign = (
-                a.m == AbstractPauli::I 
-            ||  b.m == AbstractPauli::I
-            ||  a.m == b.m) ? new_sign : !new_sign;
-    }
-    
-    if (a.m == AbstractPauli::I) { return Pauli {b.m, new_sign}; }
-    if (a.m == b.m) { return Pauli {AbstractPauli::I, new_sign}; }
-    return Pauli {
-        static_cast<AbstractPauli>(3 - (static_cast<int>(a.m) + static_cast<int>(b.m))%3),
-        new_sign};
-}
-
 /** Utility class for construction Majorana fermion representations
  */
-struct FermionRep {
-    using PauliString = std::array<Pauli, 20>;
-    using PauliMatrix = Eigen::Matrix<std::complex<short>, 2, 2>;
+struct FermionRep : PauliRep {
+protected:
     int num_fermions_;
-    
-    PauliMatrix m_[4];
 
+public:
 
-    FermionRep(int num_fermions) : num_fermions_(num_fermions) {
-        assert(num_fermions > 0);
-        assert(num_fermions % 2 == 0);
-
-        m_[0] << 1, 0,
-                0, 1;
-
-        m_[1] << 0, 1,  
-                1, 0;
-
-        m_[2] << 0, -1i,
-                1i, 0;
-
-        m_[3] << 1, 0,
-                0, -1;
+    FermionRep(int num_fermions) : PauliRep(num_fermions/2), num_fermions_(num_fermions) {
+        if(!(num_fermions > 0 && num_fermions % 2 == 0)) { throw std::runtime_error("Number of fermions must be positive and even"); }
     }
-    
-    /** Get sign
-     */
-    short repp(Sign a) const __attribute__((always_inline)) {
-        return static_cast<short>(a);
-    }
-
-    /** Get matrix for a one qubit Pauli
-     */
-    const PauliMatrix& repp(AbstractPauli a) const __attribute__((always_inline)) {
-        return m_[static_cast<short>(a)];
-    }
-
-    auto get_repp_factor(Pauli a) const __attribute__((always_inline)) {
-        return repp(a.m) * repp(a.s);
-    }
-
-    Pauli PauliI() const { return Pauli {AbstractPauli::I, Sign::P}; }
-    Pauli PauliX() const { return Pauli {AbstractPauli::X, Sign::P}; }
-    Pauli PauliY() const { return Pauli {AbstractPauli::Y, Sign::P}; }
-    Pauli PauliZ() const { return Pauli {AbstractPauli::Z, Sign::P}; }
 
     Pauli get_pauli_factor(int a, int qubit_index) {
         assert(0 <= a && a < num_fermions_);
-        assert(0 <= qubit_index && qubit_index < num_fermions_/2);
+        assert(0 <= qubit_index && qubit_index < num_qubits_);
         if (a/2 == qubit_index) { return (a%2 == 1) ? PauliY() : PauliX(); }
         if (a/2 < qubit_index)  { return PauliI(); }
         if (a/2 > qubit_index)  { return PauliZ(); }
@@ -109,7 +45,7 @@ struct FermionRep {
      */
     PauliString four_fermion(int a, int b, int c, int d) {
         PauliString pstring;
-        for(int qubit_index = 0; qubit_index < num_fermions_ / 2; ++qubit_index) {
+        for(int qubit_index = 0; qubit_index < num_qubits_; ++qubit_index) {
             pstring[qubit_index] =
                   get_pauli_factor(a, qubit_index)
                 * get_pauli_factor(b, qubit_index)
@@ -117,39 +53,6 @@ struct FermionRep {
                 * get_pauli_factor(d, qubit_index);
         }
         return pstring;
-    }
-
-    /** Single Pauli matrix element
-     */
-    std::complex<short> get_pauli_element(int i, int j, Pauli p) const __attribute__((always_inline)) {
-        return repp(p.m)(i,j) * repp(p.s);
-    }
-
-    /** Convert a short taking values 0, +/- 1 to double
-     */
-    std::complex<double> zero_one_short_to_double(std::complex<short> a) const {
-        return std::complex<double>(
-            a.real() == 0 ? 0.0 : (a.real() > 0 ? 1.0 : -1.0),
-            a.imag() == 0 ? 0.0 : (a.imag() > 0 ? 1.0 : -1.0)
-        );
-    }
-
-    /** Get matrix element of a string of Paulis
-     * shorts everywhere to hopefully let the compiler do its thing
-     */
-    std::complex<double> get_matrix_element(int i, int j, const PauliString& string) {
-        std::complex<short> acc(1,0);
-        int max_level = num_fermions_/2 - 1;
-
-        #pragma GCC unroll 4
-        for(int level = 0; level <= max_level; ++level) {
-            int mask = 1 << level;
-            int sub_i = (i & mask) >> level;
-            int sub_j = (j & mask) >> level;
-            acc *= get_pauli_element(sub_i, sub_j, string[level]);
-            if(acc.real() == 0 && acc.imag() == 0) { break; }
-        }
-        return zero_one_short_to_double(acc);
     }
 
     /** Returns the parity operator (gamma_*) for the Clifford algebra repp
@@ -163,11 +66,11 @@ struct FermionRep {
 
 /** Return all numbers of even hamming weight below size
  */
-std::vector<std::uint32_t> hamming_weight(std::uint32_t size, bool odd = false) {
+std::vector<std::uint64_t> hamming_weight(std::uint64_t size, bool odd = false) {
 
-    std::vector<std::uint32_t> idx;
+    std::vector<std::uint64_t> idx;
     idx.reserve(size/2);
-    for (std::uint32_t i = 0; i < size; ++i) {
+    for (std::uint64_t i = 0; i < size; ++i) {
         if (static_cast<bool>(__builtin_popcount(i) % 2) == odd) {
             idx.push_back(i);
         }
@@ -187,13 +90,13 @@ MatrixType syk_hamiltonian(rng_type* rng, int N, double J) {
 
     auto distr = std::normal_distribution<double>(0, J/std::pow(static_cast<double>(N), 1.5));
 
-    std::vector<std::pair<FermionRep::PauliString, double>> interactions;
+    std::vector<FermionRep::Term> interactions;
     interactions.reserve(N*N*N);
     for(int i = 0; i < N; ++i) {
         for(int j = 0; j < i; ++j) {
             for(int k = 0; k < j; ++k) {
                 for(int l = 0; l < k; ++l) {
-                    interactions.push_back(std::make_pair(repp.four_fermion(i, j, k, l), distr(*rng)));
+                    interactions.push_back(FermionRep::Term { repp.four_fermion(i, j, k, l), distr(*rng)} );
                 }
             }
         }
@@ -201,23 +104,7 @@ MatrixType syk_hamiltonian(rng_type* rng, int N, double J) {
 
     // The parity projection (1 + gamma_*) removes all rows/columns with odd hamming weight
     auto even_weight = hamming_weight(hilbert_space_size);
-    MatrixType hamiltonian = MatrixType::Zero(even_weight.size(), even_weight.size());
-
-    #pragma omp parallel for
-    for(int j = 0; j < even_weight.size(); ++j) {
-        for(int i = 0; i < even_weight.size(); ++i) {
-            double matrix_element_re = 0.0;
-            double matrix_element_im = 0.0;
-            auto num_interactions = interactions.size();
-
-            for(int k = 0; k < num_interactions; ++k) {
-                auto single_element = repp.get_matrix_element(even_weight[i], even_weight[j], interactions[k].first);
-                matrix_element_re += interactions[k].second * single_element.real();
-                matrix_element_im += interactions[k].second * single_element.imag();
-            }
-            hamiltonian(i,j) = std::complex<double>(matrix_element_re, matrix_element_im);
-        }
-    }
+    auto hamiltonian = repp.get_hamiltonian(interactions, even_weight);
 
     return hamiltonian;
 }
