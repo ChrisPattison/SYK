Partial normalization fix

src/plot/plot_spectral.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+import itertools
 import matplotlib.style
 import matplotlib as mpl
 mpl.style.use('classic')
@@ -49,7 +50,7 @@ def spectral_form_factor(output, time):
     for point in (output.Data(i) for i in range(output.DataLength())):
         spectral += spectral_form_factor(time, point.EigenvalsAsNumpy())
 
-    spectral = spectral / (output.DataLength()**2)
+    spectral = spectral / output.DataLength()
 
     return spectral, output.DataLength()
 
@@ -71,6 +72,8 @@ if __name__ == '__main__':
     time = np.geomspace(1e-1, 1e6, 4000, dtype=np.dtype('c16'))*1.0j
     output_files = sys.argv[1:]
 
+
+    colors = itertools.cycle(['b', 'r', 'c', 'm', 'k'])
     for filename in output_files:
         label = pretty_label(filename)
         print(f'Plotting {label}')
@@ -80,10 +83,9 @@ if __name__ == '__main__':
         print(f'Plotted {label} in {ti.time() - timestart}')
         plt.yscale('log')
         plt.xscale('log')
-        plt.plot(np.imag(time), np.real(spectral), label=f'${label}~~N={points}$')
-
+        plt.plot(np.imag(time), np.real(spectral), label=f'{label}$~~N={points}$', color = next(colors))
     plt.grid()
-    plt.legend()
+    plt.legend(loc='lower right')
     plt.savefig('spectral.pdf')
 
 